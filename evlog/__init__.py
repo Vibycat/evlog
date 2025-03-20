20822509916d863f7bdcd8771014092e514f3b53
Vibycat <Kylemay38@gmail.com>
2025-03-20 08:53:46 -0600

    added the function import to the __init__.py file to fix functions not importing

--- evlog/__init__.py
+++ evlog/__init__.py
@@ -1,5 +1,9 @@
 from .logger import log_event
 from .logger import calculate_total_time
+from .logger import extract_event
+from .logger import merge_logs
+from .logger import generate_summary_report
+from .logger import cleanup_old_logs
 
 __version__ = "0.0.4"
 __author__ = "Kyle May"
